af82688582dd88e6623ba852547df214c2f20eab
Gabriel Rosa <grosa.ict@gmail.com>
2019-11-06 21:18:56 -0300

    Updating classes content

---
+++ Aula14/Wallmart/Wallmart/Controllers/SellersController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Wallmart.Models;
+using Wallmart.Models.ViewModels;
+using Wallmart.Services;
+
+namespace Wallmart.Controllers
+{
+    public class SellersController : Controller
+    {   //Injeção de depêndencia do serviço de acesso aos
+        //dados de vendedores e também de departamentos
+        private readonly SellerService _sellerService;
+
+        private readonly DepartmentService _departmentService;
+
+        //Construtor para finalizar a injeção de dependência
+        public SellersController(SellerService sellerService, DepartmentService departmentService) {
+            _sellerService = sellerService;
+            _departmentService = departmentService;
+        }
+
+
+        public IActionResult Index()
+        {
+            //ICollection<Seller> Sellers = new List<Seller>();
+            //id, string name, string email, DateTime birthDate, double salary, Department department)
+            //Sellers.Add(new Seller(1, "Ana", "ana@ana.com", new DateTime(2019, 01,25), 8789.6, new Department()));
+            //return View(Sellers);
+
+            var list = _sellerService.FindAll();
+            return View(list);
+        }
+
+        public IActionResult Create(){
+            //Carrega os departamentos, utilizando o serviço de acesso
+            //aos dados de departamentos
+
+            var departments = _departmentService.FindAll();
+            //Criar uma instância de SellerFormViewModel, que vai conter
+            //duas propriedades, a primeira é o lista de departamentos e
+            //a outra será o Seller.
+
+            var viewModel =
+                new SellerFormViewModel { Departments = departments };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(SellerFormViewModel obj)
+        {
+            Seller slr = obj.Seller;
+            _sellerService.Insert(slr);
+            return RedirectToAction("Index");
+        }
+
+        // int? significa que o parâmetro id pode receber nulo, ou seja, é opcional
+        public IActionResult Delete(int? id)
+        {
+            // testar com !id, ou seja, verifica se o usuário tentou acessar a url sem indicar o id
+            if (id == null)
+            {
+                // trata o erro
+                return NotFound();
+            }
+
+            // Tenta buscar o registro desse vendedor
+            var obj = _sellerService.FindById(id.Value);
+
+            // Verifica se foi encontrado um Seller com esse id
+            if (obj == null)
+            {
+                // Se não encontrado retorna página de não encontrado
+                return NotFound();
+            }
+
+            // Retorna vendedor encontrado
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Aula14/Wallmart/Wallmart/Services/SellerService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wallmart.Models;
+
+namespace Wallmart.Services
+{
+    public class SellerService
+    {
+        /*declara um objeto que é o contexto do banco*/
+        private readonly WallmartContext _context;
+
+        public SellerService(WallmartContext context) {
+            _context = context;
+        }
+
+        public List<Seller> FindAll() {
+            /*Acessa a tabela Seller e transforma os
+             registros em objetos e retorna como uma
+             lista de objetos*/
+            return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj) {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            // retorna o vendedor do o ID recebido por parâmetro
+            return _context.Seller.FirstOrDefault(s => s.Id == id);
+
+            /* _context é a conexão com o banco
+             * FirstOrDefault é uma função que irá buscar o valor passado no banco
+             * s => cria um objeto identico ao registro do banco
+             * s.Id acessa o campo Id do registro
+             * == id compara o valor anterior com o id passado por parâmetro
+             * Quando encontrado, retorna o registro inteiro
+             * Senão retorna NULL que é a característica do OrDefault da função
+             */
+        }
+
+        public void Remove(int id)
+        {
+            // Cria uma instância de Seller para apontar para o registro que quero remover
+            var seller = _context.Seller.Find(id);
+
+            // em tese faz o mesmo que o comando anterior
+            // var seller = FindById(id);
+
+            // Remove o objeto do banco de dados
+            _context.Seller.Remove(seller);
+
+            // Confirmar a transação de exclusão do registro ao EntityFramework para persistir a alteração
+            _context.SaveChanges();
+        }
+    }
+}
